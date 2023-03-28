ffedad721464d6a63dee797fb9361cc68cda834c
elena <elender1230@gmail.com>
2023-03-28 16:58:11 +0200

    WIP

--- lab_09_modules/01_using_third_party/lab_09a_vpc/ec2.tf
+++ lab_09_modules/01_using_third_party/lab_09a_vpc/ec2.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "server_pub" {
 
 locals {
   num_pub_subnets = length(module.vpc_one.public_subnets)
+  num_pub_subnets_otra_vpc = length(module.otra_vpc.public_subnets)
 }
 
 resource "aws_instance" "server_priv" {
@@ -18,6 +19,17 @@ resource "aws_instance" "server_priv" {
   ami           = data.aws_ami.amazon_linux2_kernel_5.id
   instance_type = var.instance_type
   subnet_id     = module.vpc_one.private_subnets[count.index]
+  tags = {
+    Name = "vm-priv-${local.name_suffix}-${count.index}"
+  }
+}
+
+
+resource "aws_instance" "second_server" {
+  count         = local.num_pub_subnets_otra_vpc
+  ami           = data.aws_ami.amazon_linux2_kernel_5.id
+  instance_type = var.instance_type
+  subnet_id     = module.otra_vpc.public_subnets[count.index]
   tags = {
     Name = "vm-priv-${local.name_suffix}-${count.index}"
   }
